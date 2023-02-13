6d45b5758ea433142935575978ee5b0d3c847637
Oliwia Kwiatek <livkwiatek@gmail.com>
2023-02-12 19:29:32 -0500

    sprint1

--- index.js
+++ index.js
@@ -208,17 +208,20 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
+console.log("task 3:",artists[0].name)
 
 
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log("task 2:",artists[2].name)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 (not auto tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
+artists[8].bio = 'Vincent Van Doug'
+console.log(artists[8].bio)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -230,9 +233,15 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artistArr, index){
+let artist = artistArr[index]
+ let artistName = artist.name;
+
+return `the artist at index ${index} is ${artistName}`
 }
+console.log('Task3:', getArtistByIndex(artists, 0))
+
+
 
 
 
@@ -244,9 +253,15 @@ Use listOfNames to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(/*Your Code Here*/) {
-  /*Your Code Here*/
+function listOfNames(artistArr) {
+  let artistArr2 = [...artistArr]
+  for(let i = 0; i < artistArr.length; i++){
+let currentArtistName = artistArr2[i].name
+artistArr2[i] = currentArtistName
+  }
+  return artistArr2
 }
+console.log('task 4:',listOfNames(artists))
 
 
 
@@ -259,9 +274,12 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(artistArr, index) {
+  let artistArr2 = [...artistArr];
+  artistArr2.splice(index, 1)
+  return artistArr2
 }
+console.log("Task 5:", removeArtist(artists, 0))
 
 
 
@@ -281,9 +299,20 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(artistArr, name, years, genre, nationality, bio) {
+  let artistArr2 = [...artistArr];
+  let newArtist = {
+    name: name,
+    years: years,
+    genre: genre,
+    nationality: nationality,
+    bio: bio,
+  }
+
+artistArr2.push(newArtist);
+return artistArr2
 }
+console.log("task 6:", addArtist(artists, "Oliwia Kwiatek", "2021-2022", "web dev", "Polish", " I'm an accountant, I love to invest in crypto and NFT's and I'd love to be a web dev" ))
 
 
 
@@ -295,8 +324,16 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(artistArr) {
+  let lotsOfArtName = []
+  for(let i = 0; i < artistArr.length; i++){
+    let currentArtist = artistArr[i].paintings
+    if(currentArtist > 100) {
+      lotsOfArtName.push(artistArr[i].name)
+    }
+
+  }
+  return lotsOfArtName
 }
 
 
@@ -311,9 +348,15 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(artistArr, artistName){
+for(let i = 0; i < artistArr.length; i++){
+  let currentArtistName = artistArr[i].name
+  if(currentArtistName == artistName){
+    return artistArr[i].bio
+  }
 }
+}
+console.log("Task 8:", artistInfo(artists, "Frida Kahlo"))
 
 
 
@@ -327,9 +370,24 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
+function artistByCountry(artistArr, nationality){
+  let artistNames = []
+  for(let i = 0; i < artistArr.length; i++){
+    let currentNationality = artistArr[i].nationality
+    currentNationality = currentNationality.split(',')
+    if(currentNationality.length > 1) {
+      continue
+    }
+  if(currentNationality[0] == nationality){
+    artistNames.push(artistArr[i].name)
+  }
   /*Your Code Here*/
 }
+return artistNames
+}
+
+
+console.log("Task 9:", artistByCountry(artists, "Spanish"))
 
 
 
